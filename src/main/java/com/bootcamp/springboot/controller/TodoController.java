a1fd59f00ef6d03418be8f8bf0a7daea16052dda
Victor Leandro R. Dela Cruz <vldelacruz@teligent.com.ph>
2021-02-11 19:57:27 +0800

    added crud

--- src/main/java/com/bootcamp/springboot/controller/TodoController.java
+++ src/main/java/com/bootcamp/springboot/controller/TodoController.java
@@ -1,19 +1,15 @@
 package com.bootcamp.springboot.controller;
 
 
-import com.bootcamp.springboot.config.DatabaseConfig;
 import com.bootcamp.springboot.model.Todo;
-import com.bootcamp.springboot.service.DatabaseService;
 import com.bootcamp.springboot.service.TodoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Controller
 @RequestMapping("todo")
@@ -27,8 +23,42 @@ public class TodoController {
     }
 
     @GetMapping("/list")
-    public ResponseEntity<ArrayList<Todo>> List() {
-        ArrayList<Todo> result  = this.todoService.getAllTodos();
-        return new ResponseEntity<ArrayList<Todo>>(result, HttpStatus.OK);
+    public String List(Model model) {
+        model.addAttribute("todoList",this.todoService.getAllTodos());
+        return "list";
+    }
+
+    @GetMapping("/create")
+    public String create(Model model) {
+        model.addAttribute("todo",new Todo());
+        model.addAttribute("action","create");
+        return "create";
+    }
+
+    @GetMapping("/edit")
+    public String update(@ModelAttribute Todo todo,Model model) {
+        model.addAttribute("todo",todoService.getTodoById(todo));
+        model.addAttribute("action","edit");
+        return "create";
+    }
+
+
+
+    @PostMapping("/add-todo")
+    public String add(@ModelAttribute Todo todo,Model model) {
+        todoService.AddNewTodo(todo);
+        return "redirect:/todo/list";
+    }
+
+    @PostMapping("/edit-todo")
+    public String editTodo(@ModelAttribute Todo todo,Model model) {
+        todoService.setTodoDescription(todo);
+        return "redirect:/todo/list";
+    }
+
+    @GetMapping("/delete-todo")
+    public String delete(@ModelAttribute Todo todo,Model model) {
+        todoService.deleteTodo(todo);
+        return "redirect:/todo/list";
     }
 }
--- src/main/java/com/bootcamp/springboot/service/TodoService.java
+++ src/main/java/com/bootcamp/springboot/service/TodoService.java
@@ -11,6 +11,7 @@ import java.util.ArrayList;
 @Service
 public class TodoService {
 
+    public static ArrayList<Todo> todoList = new ArrayList<>();
     DatabaseService databaseService;
 
     @Autowired
@@ -20,14 +21,46 @@ public class TodoService {
 
     public ArrayList<Todo> getAllTodos() {
         String conn = this.databaseService.getDatabaseConnection();
+        return todoList;
+    }
 
-        //mock select from table
-        ArrayList<Todo> list = new ArrayList<>();
-        list.add(new Todo(1,"Buy school supplies"));
-        list.add(new Todo(2,"Do modules"));
-        list.add(new Todo(3,"Get a life"));
+    public Todo getTodoById(Todo todo) {
+        for (Todo item : todoList) {
+            if (item.getId() == todo.getId()) {
+                return item;
+            }
+        }
+        return todo;
+    }
 
-        return list;
+    public void setTodoDescription(Todo todo) {
+        for (Todo item : todoList) {
+            if (item.getId() == todo.getId()) {
+                item.setDescription(todo.getDescription());
+            }
+        }
+    }
+
+
+    public void deleteTodo(Todo todo) {
+        Todo todo1 = new Todo();
+        for (Todo item : todoList) {
+            if (item.getId() == todo.getId()) {
+                todo1 = item;
+            }
+        }
+        todoList.remove(todo1);
+    }
+
+
+    public void AddNewTodo(Todo todo) {
+
+        if (todoList.size() == 0) {
+            todo.setId(1);
+        } else {
+            todo.setId(todoList.get(todoList.size() - 1).getId() + 1);
+        }
+        todoList.add(todo);
     }
 
 }
